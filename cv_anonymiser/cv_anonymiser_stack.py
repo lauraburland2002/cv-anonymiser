e9fffa0d7b41befd26841ce690a80f2b3e4bb3bb
lauraburland2002 <lauraburland2@outlook.com>
2025-12-20 10:26:05 +0000

    Initial commit: CV Anonymiser

--- cv_anonymiser/cv_anonymiser_stack.py
+++ cv_anonymiser/cv_anonymiser_stack.py
@@ -1,19 +1,36 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    CfnOutput,
+    aws_lambda as lambda_,
+    aws_apigateway as apigateway,
 )
 from constructs import Construct
+
 
 class CvAnonymiserStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Lambda function
+        cv_lambda = lambda_.Function(
+            self,
+            "CvAnonymiserFunction",
+            runtime=lambda_.Runtime.PYTHON_3_11,
+            handler="index.main",
+            code=lambda_.Code.from_inline(
+                "def main(event, context): "
+                "return {'statusCode': 200, 'body': 'CV anonymiser running'}"
+            ),
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CvAnonymiserQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # API Gateway in front of the Lambda
+        api = apigateway.LambdaRestApi(
+            self,
+            "CvAnonymiserApi",
+            handler=cv_lambda,
+        )
+
+        # Outputs
+        CfnOutput(self, "ApiUrl", value=api.url)
+        CfnOutput(self, "LambdaName", value=cv_lambda.function_name)
